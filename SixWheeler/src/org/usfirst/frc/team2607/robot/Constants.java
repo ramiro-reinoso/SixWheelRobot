9ba53f7e9fe5af007445386fccc19e91898cafa6
Ramiro <ramiro.reinoso@verizon.net>
2018-05-21 23:31:45 -0400

    rer- Third Change

--- SixWheeler/src/org/usfirst/frc/team2607/robot/Constants.java
+++ SixWheeler/src/org/usfirst/frc/team2607/robot/Constants.java
@@ -15,6 +15,7 @@ public class Constants {
 	public static final int testvariable33 = 33;
 	public static final int testvariable34 = 34;
 	public static final int testvariable35 = 35;
+	public static final int testvariable36 = 36;  // Added by rer
 
 	
 	public static final int shifterSolenoid = 0;
